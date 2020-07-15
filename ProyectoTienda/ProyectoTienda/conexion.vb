91ce449f3b1e1dede2e54ed1d5f0f8cc2d3c139a
Samuel Oviedo <isaaccoviedo@gmail.com>
2020-07-15 01:13:29 -0600

    conexionProyectoClase

---
+++ ProyectoTienda/ProyectoTienda/conexion.vb
@@ -0,0 +1,14 @@
+﻿Imports System.Data.SqlClient
+Public Class conexion
+    Public conexion As SqlConnection = New SqlConnection("Data Source=DESKTOP-O55QRS2\MSSQLSERVERSAM;Initial Catalog=Tienda;Integrated Security=True")
+    Public Sub abrirConexion()
+        Try
+            conexion.Open()
+        Catch ex As Exception
+            MessageBox.Show("No se pudo Conectar: " + ex.ToString)
+            End
+        Finally
+            conexion.Close()
+        End Try
+    End Sub
+End Class
